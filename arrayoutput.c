5182a832f406feb957b5491409b96ace5cf44eec
anjanaravi105 <anjanaravi053@gmail.com>
2025-08-30 12:20:25 -0700

    Added arrayoutput.c file

---
+++ arrayoutput.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a[1000];
+    int i,limit;
+    printf("Enter array limit");
+    scanf("%d",&limit);
+    printf("Enter values");
+    for(i=0;i<limit;i++){
+        scanf("%d",&a[i]);
+    }
+
+    printf("Entered values are: ");
+
+    for(i=0;i<limit;i++){
+        printf("%d\t",a[i]);
+
+    }
+    return 0;
+
+}
+
